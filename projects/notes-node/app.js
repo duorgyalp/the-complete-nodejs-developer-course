7ddf5d4463bc6c52af60dfa574b2d962f9197370
Duorgyalp <duorgyalp.ymranootgnok@gmail.com>
2018-07-27 14:02:59 +0700

    11. Using 3rd Party Modules

--- projects/notes-node/app.js
+++ projects/notes-node/app.js
@@ -2,9 +2,15 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result:', notes.add(9, -2));
+// console.log(_.isString(true));
+// console.log(_.isString('Duorgyalp'));
+var filterArray = _.uniq(['Duorgyalp', 1, 'Duorgyalp', 1, 2, 3, 4]);
+console.log(filterArray);
+
+//console.log('Result:', notes.add(9, -2));
 
 //var user = os.userInfo();
 
